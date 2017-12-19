0cded1c262c9b6614b46522ccddd1a7c33fecbb8
John Patterson <pattersonjc1@gmail.com>
2017-12-19 15:52:29 -0500

    finished tutorial

--- tutorial-part-four/src/pages/index.js
+++ tutorial-part-four/src/pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import g from 'glamorous';
+import Link from 'gatsby-link';
 
 import { rhythm } from '../utils/typography';
 
 export default ({ data }) => {
-	console.log(data);
 	return (
 		<div>
 			<g.H1 display={'inline-block'} borderBottom={'1px solid'}>
@@ -13,11 +13,18 @@ export default ({ data }) => {
 			<h4>{data.allMarkdownRemark.totalCount} Posts</h4>
 			{data.allMarkdownRemark.edges.map(({ node }) => (
 				<div key={node.id}>
-					<g.H3 marginBottom={rhythm(1 / 4)}>
-						{node.frontmatter.title}{' '}
-						<g.Span color="#BBB">— {node.frontmatter.date}</g.Span>
-					</g.H3>
-					<p>{node.excerpt}</p>
+					<Link
+						to={node.fields.slug}
+						css={{ textDecoration: `none`, color: `inherit` }}
+					>
+						<g.H3 marginBottom={rhythm(1 / 4)}>
+							{node.frontmatter.title}{' '}
+							<g.Span color="#BBB">
+								— {node.frontmatter.date}
+							</g.Span>
+						</g.H3>
+						<p>{node.excerpt}</p>
+					</Link>
 				</div>
 			))}
 		</div>
@@ -26,7 +33,7 @@ export default ({ data }) => {
 
 export const query = graphql`
 	query IndexQuery {
-		allMarkdownRemark {
+		allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
 			totalCount
 			edges {
 				node {
@@ -34,6 +41,9 @@ export const query = graphql`
 					frontmatter {
 						title
 						date(formatString: "DD MMMM, YYYY")
+					}
+					fields {
+						slug
 					}
 					excerpt
 				}
